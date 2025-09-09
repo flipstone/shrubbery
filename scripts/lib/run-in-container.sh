2ce4331f010b3b128efb67fdda4d7b135f1b6de0
David Vollbracht <david@flipstone.com>
2025-09-09 10:57:22 -0500

    Updates matrix build to use shared actions
    This includes using the `!include` pattern in the `stack.yaml` files
    to reduce the duplication and follow the pattern that the shared
    action for discovering the stack files for the matrix build expects.

--- scripts/lib/run-in-container.sh
+++ scripts/lib/run-in-container.sh
@@ -5,5 +5,5 @@ else
   # Script was run from outside the container, re-exec inside the container
   # with the same arguments
   docker compose build
-  exec docker compose run --rm shrubbery $0 "$@"
+  exec docker compose run --rm dev $0 "$@"
 fi
